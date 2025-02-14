ce3ca0b8d09094fff080345d6c44d9417a4d87ed
Alena-pa <alena.pakhnusheva@gmail.com>
2025-02-14 15:35:18 +0300

    write smth with strings

--- ClassWork/ClassWork/Program.cs
+++ ClassWork/ClassWork/Program.cs
@@ -1,27 +1,78 @@
-﻿var array = new int[10];
+﻿using System.Net.WebSockets;
+using System.Runtime.Intrinsics.X86;
+
+var array = new int[10];
+
+var str = System.Console.ReadLine();
+if (str is null)
+{
+    Console.WriteLine("ВСЕ ПЛОХО");
+    return -1;
+}
+
+var массив = str.Split(' ');
+
+var array2 = массив.Select(x => int.Parse(x)).ToArray();
+
+for (int i = 0; i < array2.Length; i++)
+{
+    if (int.TryParse(массив[i], out int result))
+    {
+        array2[i] = result;
+    }
+}
+
+for (var i = array.Length - 1; i >= 0; i--)
+{
+    Console.Write($"{array2[i]} ");
+}
+Console.WriteLine();
+
 for (var i = array.Length - 1; i >= 0; i--)
 {
     array[i] = i * i;
 }
+
+BubbleSort(array);
 
 for (var i = 0; i < array.Length; i++)
 {
     System.Console.Write($"{array[i]} ");
 }
 
+var (mean, dispertion) = Math(array);
+
+System.Console.WriteLine($"\nМат ожидание: {mean}\nДисперсия: {dispertion}");
+
+(double Mean, double Dispertion) Math(int[] array)
+{
+    double mean = calcMean(array);
+    double dispersion = 1;
+    for (var i = 0; i < array.Length; i++)
+    {
+        dispersion += System.Math.Pow((array[i] - mean), 2.0);
+    }
+    dispersion = dispersion / (array.Length - 1);
+
+    return (mean, dispersion);
+
+    float calcMean (int[] array) => array.Sum() / (float)array.Length;
+}
 void BubbleSort(int[] array)
 {
     bool isArraySorted = false;
     while (!isArraySorted)
     {
         isArraySorted = true;
-        for (var i = 0; i < array.Length; i++)
+        for (int i = 0; i < array.Length - 1; i++)
         {
-            if (array[i - 1] > array[i])
+            if (array[i] > array[i + 1])
             {
-                (array[i - 1], array[i]) = (array[i], array[i - 1]);
+                (array[i], array[i + 1]) = (array[i + 1], array[i]);
                 isArraySorted = false;
             }
         }
     }
 }
+
+return 0;
--- ClassWork/ClassWork/obj/Debug/net8.0/ClassWork.AssemblyInfo.cs
+++ ClassWork/ClassWork/obj/Debug/net8.0/ClassWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d02461d2d6dd1341c1731707b8a994f38bb783")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
