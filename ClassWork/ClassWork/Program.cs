01d02461d2d6dd1341c1731707b8a994f38bb783
Alena-pa <alena.pakhnusheva@gmail.com>
2025-02-14 14:42:00 +0300

    create BubbleSort

---
+++ ClassWork/ClassWork/Program.cs
@@ -0,0 +1,27 @@
+﻿var array = new int[10];
+for (var i = array.Length - 1; i >= 0; i--)
+{
+    array[i] = i * i;
+}
+
+for (var i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"{array[i]} ");
+}
+
+void BubbleSort(int[] array)
+{
+    bool isArraySorted = false;
+    while (!isArraySorted)
+    {
+        isArraySorted = true;
+        for (var i = 0; i < array.Length; i++)
+        {
+            if (array[i - 1] > array[i])
+            {
+                (array[i - 1], array[i]) = (array[i], array[i - 1]);
+                isArraySorted = false;
+            }
+        }
+    }
+}
